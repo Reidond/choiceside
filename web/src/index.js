cbbd3037ae4788f646f0fadafc663769440f72c5
Reidond <reidond@gmail.com>
2020-10-06 09:44:51 +0300

    chore: add packages
    - recoil
    - @blueprintjs/core
    - @blueprintjs/icons
    - @blueprintjs/select
    - @blueprintjs/table

--- web/src/index.js
+++ web/src/index.js
@@ -1,16 +1,24 @@
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
+import { RecoilRoot } from 'recoil'
 
 import Routes from 'src/Routes'
 
 import './index.css'
+import 'normalize.css/normalize.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+import '@blueprintjs/select/lib/css/blueprint-select.css'
+import '@blueprintjs/table/lib/css/table.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
 
 ReactDOM.render(
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider>
-      <Routes />
-    </RedwoodProvider>
-  </FatalErrorBoundary>,
+  <RecoilRoot>
+    <FatalErrorBoundary page={FatalErrorPage}>
+      <RedwoodProvider>
+        <Routes />
+      </RedwoodProvider>
+    </FatalErrorBoundary>
+  </RecoilRoot>,
   document.getElementById('redwood-app')
 )
