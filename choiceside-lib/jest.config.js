22d84e65a1700abe72628f49db5755ed41e37580
Reidond <reidond@gmail.com>
2020-10-15 15:47:10 +0300

    chore: ignore dist folder for lib tests

--- choiceside-lib/jest.config.js
+++ choiceside-lib/jest.config.js
@@ -4,7 +4,12 @@ export default {
     '**/__tests__/**/*.+(ts|tsx|js)',
     '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  displayName: {
+    name: 'choiceside-lib',
+    color: 'magentaBright',
   },
 }
